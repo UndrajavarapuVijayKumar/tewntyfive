a5455835d7e8301b5bf5dd37a6ce325c3c5fb4a0
VijayUndrajavarapu <54445371+VijayUndrajavarapu@users.noreply.github.com>
2025-03-07 20:41:05 +0530

    prime

--- src/sam/prime.java
+++ src/sam/prime.java
@@ -3,8 +3,22 @@ package sam;
 public class prime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num =7;
+		if(isPrime(num)) {
+			System.out.println(num + " is a prime number");
+		}else {
+			System.out.println(num + "is not a prime ");
+		}
 	}
-
+	public static boolean isPrime(int num) {
+		if(num<2) {
+			for(int i=2;i<=num;i++) {
+				if(num%i==0) {
+					return false;
+				}
+			}
+			
+		}
+		return true;
+	}
 }
