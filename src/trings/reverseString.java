ad9fde66022f1000b53ded3b738d37608a493b8d
VijayUndrajavarapu <54445371+VijayUndrajavarapu@users.noreply.github.com>
2025-03-09 02:04:02 +0530

    strings

--- src/trings/reverseString.java
+++ src/trings/reverseString.java
@@ -6,7 +6,8 @@ public class reverseString {
 		String s="vijay";
 		StringBuffer sb = new StringBuffer(s);
 		System.out.println(sb.reverse());
-
+		System.out.println("String buffers are introduced in java 1.0"+"\n"+
+		"String buffers are thread safe due to which these are slow ");
 	}
 
 }
