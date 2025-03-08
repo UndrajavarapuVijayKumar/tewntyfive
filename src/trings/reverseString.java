d0a41a3e5c00ef75f451750b705f95d01f404441
VijayUndrajavarapu <54445371+VijayUndrajavarapu@users.noreply.github.com>
2025-03-09 01:52:43 +0530

    strings

---
+++ src/trings/reverseString.java
@@ -0,0 +1,12 @@
+package trings;
+
+public class reverseString {
+
+	public static void main(String[] args) {
+		String s="vijay";
+		StringBuffer sb = new StringBuffer(s);
+		System.out.println(sb.reverse());
+
+	}
+
+}
